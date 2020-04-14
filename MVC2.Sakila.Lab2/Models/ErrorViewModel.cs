462866a1474bacb1d94731a119b5fbe142e5f88b
47720480 <david.n.stahl@gmail.com>
2020-04-14 15:18:17 +0200

    first commit

---
+++ MVC2.Sakila.Lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC2.Sakila.Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
